cafe7e426adbf60d348389583c35cd045d63e4e3
Nick Hopgood <63095611+SirHoppington@users.noreply.github.com>
2026-07-20 16:04:56 +0100

    Create main.go

---
+++ go/cmd/server/main.go
@@ -0,0 +1,33 @@
+// Command server is a tiny HTTP server used to demonstrate a Go
+// multi-stage Docker build: compile here, then ship the single
+// static binary on a scratch/distroless base.
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello from a multi-stage Docker build!")
+	})
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintln(w, "ok")
+	})
+
+	addr := ":" + port
+	log.Printf("listening on %s", addr)
+	if err := http.ListenAndServe(addr, mux); err != nil {
+		log.Fatal(err)
+	}
+}
